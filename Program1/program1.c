50a82e697263f7459ebd47ec3dd3c7fd8bb85c78
irfan hanif <if.irfanhanif@gmail.com>
2015-11-09 16:04:51 +0700

    update program1: akses /bin dan /bin/usr

--- Program1/program1.c
+++ Program1/program1.c
@@ -1,15 +1,51 @@
 #include <stdio.h>
 #include <signal.h>
+#include <unistd.h>
+#include <string.h>
 
 void sigHandler(int sigNumber){
 	printf("\nYou cannot interrupt/stop this program.\n");
-	printf("press 0 to quit\n");
+	printf("input 0 to quit\n");
 }
 
 void main(){
 	signal(SIGINT, sigHandler);
 	signal(SIGTSTP, sigHandler);
-	int a;
-	scanf("%d", &a);
-	if(a==0) return;
+
+	pid_t pid, temp;
+
+	char input[10];
+	while(1){
+		memset(input, '\0', sizeof(input));
+		printf("Masukkan perintah: ");
+		scanf("%[^\n]", input);
+		getchar();
+		if(input[0] == 'q') break;
+		//printf("%s\n", input);
+
+		char test[5][100];
+		int i = 0;
+
+		memset(test, '\0', sizeof(test));
+
+		char *token = strtok(input, " ");
+
+		while(token != NULL){
+			strcpy(test[i], token);
+			i++;
+			token = strtok(NULL, " ");
+		}
+
+		pid = fork();
+		if(pid == 0){
+			if(i>1)
+				execlp(test[0], test[0], test[1], (char*) NULL);
+			else{
+				execlp(test[0], test[0], (char*) NULL);
+			}
+			//temp = getpid();
+		}
+		//waitpid(temp);
+		wait();
+	}
 }
