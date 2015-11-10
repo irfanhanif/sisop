9d39c39f2345da3bbc1a11fb172ea264433c50a6
irfan hanif <if.irfanhanif@gmail.com>
2015-11-11 00:36:28 +0700

    update program1: ctrl+d dan menampilkan active directory

--- Program1/program1.c
+++ Program1/program1.c
@@ -5,31 +5,37 @@
 
 void sigHandler(int sigNumber){
 	printf("\nYou cannot interrupt/stop this program.\n");
-	printf("input 0 to quit\n");
+	printf("Press CTRL + D to quit\n");
 }
 
 void main(){
 	signal(SIGINT, sigHandler);
 	signal(SIGTSTP, sigHandler);
 
-	pid_t pid;
+	pid_t pid, start;
 
 	int i; 
 	char *token, *test[5], input[10], *x = "cd", *dan = "&";
+	char *substr;
+	char ch;
 
-	while(1){
-		memset(input, '\0', sizeof(input));
-		printf("Masukkan perintah: ");
-		scanf("%[^\n]", input);
-		if(EOF) return;
+	start = fork();
+	if (start == 0){
+		execlp("pwd", "pwd", NULL);
+	}
+	wait();
+	printf("Input: ");
+	while(scanf("%[^\n]", input)!= EOF){
+		//memset(input, '\0', sizeof(input));
+		//printf("Masukkan perintah: ");
 		getchar();
-		
-		//if(input[0] == '\04') break;
+
 
 		i = 0;
 
 		memset(test, '\0', sizeof(test));
 		token = strtok(input, " ");
+		substr = strstr(input, " ");
 
 		while(token != NULL){
 			test[i] = token;
@@ -39,6 +45,11 @@ void main(){
 
 		if(strcmp(test[0], x) == 0){
 			chdir(test[i-1]);
+			start = fork();
+			if(start == 0){
+				execlp("pwd", "pwd", NULL);
+			}
+			wait();
 		}
 		else{
 			if(strcmp(test[i-1], dan) == 0){
@@ -47,6 +58,11 @@ void main(){
 				if(pid == 0){
 					execvp(test[0], test);
 				}
+				start = fork();
+				if(start == 0){
+					execlp("pwd", "pwd", NULL);
+				}
+				wait();
 			}
 			else{
 				pid = fork();
@@ -54,7 +70,14 @@ void main(){
 					execvp(test[0], test);
 				}
 				wait();
+				start = fork();
+				if(start == 0){
+					execlp("pwd", "pwd", NULL);
+				}
+				wait();
 			}
 		}
+		memset(input, '\0', sizeof(input));
+		printf("Input: ");
 	}
 }
